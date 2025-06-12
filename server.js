85cf2d8ad93ac620c35825b09328cdf0eeb820c3
romeyiow <jimperialyosh@gmail.com>
2025-06-13 06:43:11 +0800

    dev: adjusted rate limit to 1000 request per IP for development

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true })); // Parses incoming requests wit
 // Rate Limiting Configuration
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes window
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 1000, // Limit each IP to 100 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers (legacy)
     // Message sent when rate limit is exceeded
